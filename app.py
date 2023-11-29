82d5a6f0e183ad5fe41356f423ae69ed3d2cbffa
leemim <105574870+WorldHello13@users.noreply.github.com>
2023-11-29 16:09:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def generate_frames():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('/video_feed')
 def video_feed():
